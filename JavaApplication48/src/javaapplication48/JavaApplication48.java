45412cf74ce4436d1e266fccfa9871939908b64d
minionMike <morten_skar@hotmail.no>
2015-09-16 14:31:52 +0200

    morten test2

--- JavaApplication48/src/javaapplication48/JavaApplication48.java
+++ JavaApplication48/src/javaapplication48/JavaApplication48.java
@@ -17,6 +17,6 @@ public class JavaApplication48
      */
     public static void main(String[] args)
     {
-        System.out.println("test");    }
+        System.out.println("test2");    }
     
 }
