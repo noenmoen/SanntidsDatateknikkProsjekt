39eb996ba7ad361632890ac45d6e958024a190cf
noenmoen <martinspedersen1@hotmail.com>
2015-10-06 16:35:33 +0200

    testa noen parametere. automatisk avslutning av program etter gitt tid.

--- PS3ControllerData/src/yadrone/YADrone.java
+++ PS3ControllerData/src/yadrone/YADrone.java
@@ -20,12 +20,14 @@ import java.util.logging.Logger;
  *
  * @author vegard
  */
-public class YADrone {
+public class YADrone
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Timer timer = new Timer();
         Semaphore mySem = new Semaphore(1, true);
         ControllerStateStorage store = new ControllerStateStorage();
@@ -34,27 +36,28 @@ public class YADrone {
         try {
             drone = new ARDrone();
             drone.start();
-        } catch (Exception exc) {
+        }
+        catch (Exception exc) {
             exc.printStackTrace();
         }
-        
         drone.getCommandManager().setVideoBitrateControl(VideoBitRateMode.DISABLED); // Test this        
-        drone.getCommandManager().setVideoCodec(VideoCodec.H264_720P); // Test this
-        drone.getCommandManager().setVideoBitrate(4000); // Test (max bitrate)
-        
+        drone.getCommandManager().setVideoCodec(VideoCodec.H264_360P); // Test this
+
         CircleDetection cd = new CircleDetection(1000, 30, 3, 13, 204, 200, 3, drone, 3);
         cd.start();
-        DroneGUI gui = new DroneGUI(drone);
-        gui.start();
-        
-        try {
-            reader = new PS3ControllerReader(mySem, store);
-        } catch (IOException ex) {
-            Logger.getLogger(YADrone.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        timer.scheduleAtFixedRate(reader, 0, 5);
-        DroneControl cont = new DroneControl(drone, mySem, store);
-        timer.scheduleAtFixedRate(cont, 0, 5);
-        
+//        DroneGUI gui = new DroneGUI(drone);
+//        gui.start();
+        long startTime = System.currentTimeMillis();
+        while(startTime>(System.currentTimeMillis()-20*1000)){}
+        drone.stop();
+        System.exit(0);
+//        try {
+//            reader = new PS3ControllerReader(mySem, store);
+//        } catch (IOException ex) {
+//            Logger.getLogger(YADrone.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        timer.scheduleAtFixedRate(reader, 0, 5);
+//        DroneControl cont = new DroneControl(drone, mySem, store);
+//        timer.scheduleAtFixedRate(cont, 0, 5);
     }
 }
