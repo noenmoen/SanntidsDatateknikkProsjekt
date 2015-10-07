f0fb241370dac21a1383f40be655b1e8575c2935
minionMike <morten_skar@hotmail.no>
2015-10-07 15:07:41 +0200

    Regulator class

--- PS3ControllerData/src/yadrone/Regulator.java
+++ PS3ControllerData/src/yadrone/Regulator.java
@@ -15,23 +15,25 @@ import org.opencv.core.Mat;
 public class Regulator {
 
     private ArrayList<Mat> rawCoordinates;
+    private ArrayList<double[]> filteredCoordinates;
+    private final double CircleDiameter = 83;
 
     public Regulator() {
 
     }
 
     /**
-     * 
+     *
      * Returns the average from the 6 last coordinates
-     * @return 
+     *
      */
-    public double[] CoordinateFilter() {
+    public void CoordinateFilter() {
         int elements = 6;
         double[] avg = new double[3];
         double sumX = 0;
         double sumY = 0;
         double sumR = 0;
-        for (int i = 0; i < (elements-1); i++) {
+        for (int i = 0; i < (elements - 1); i++) {
             sumX = sumX + (double) rawCoordinates.get(i).get(0, 0)[0];
             sumY = sumY + (double) rawCoordinates.get(i).get(0, 0)[1];
             sumR = sumR + (double) rawCoordinates.get(i).get(0, 0)[2];
@@ -39,14 +41,30 @@ public class Regulator {
         avg[0] = sumX / elements;
         avg[1] = sumY / elements;
         avg[2] = sumR / elements;
-        return avg;
+        filteredCoordinates.add(avg);
+        if (rawCoordinates.size() > elements) {
+            rawCoordinates.remove(elements);
+        }
     }
 
+    /**
+     * Add a new raw coordinate
+     * @param v
+     */
     public void AddNewCoordinate(Mat v) {
-        rawCoordinates.add(v);
+        if (!v.empty() && v.cols() == 1 && v.rows() == 1) {
+            rawCoordinates.add(v);
+        }
 
         if (rawCoordinates.size() - 1 > 5) {
             rawCoordinates.remove(6);
         }
     }
+    
+    public void DistanceEstimate(double[] a){
+        double realRad = CircleDiameter/2;
+        double virtualRadius = a[3];
+        
+    }
+    
 }
