dccc375ac8f8ebb2680a1e6af8bbcac3d68a445c
vegkams <vegard.kamsvag@stud.hials.no>
2015-10-14 14:54:50 +0200

    Started on PID-algorithms, work in progress

--- PS3ControllerData/src/yadrone/Regulator.java
+++ PS3ControllerData/src/yadrone/Regulator.java
@@ -17,9 +17,27 @@ public class Regulator {
     private ArrayList<Mat> rawCoordinates;
     private ArrayList<double[]> filteredCoordinates;
     private final double CircleDiameter = 83;
-
+    private float[] droneInputs = new float[4];
+    private float yawErr;
+    private float yawErrSum;
+    private float yawDerr;
+    private float prevYawErr;
+    private float kpYaw;
+    private float kiYaw;
+    private float kdYaw;
+    private float yawSpeedOut;
+    private float zErr;
+    private float zErrSum;
+    private float zDerr;
+    private float prevZerr;
+    private float kpZ;
+    private float kiZ;
+    private float kdZ;
+    private float zSpeedOut;
+    private final float TIME_SHIFT;
+    
     public Regulator() {
-
+        TIME_SHIFT = 0.1f;
     }
 
     /**
@@ -72,4 +90,34 @@ public class Regulator {
         
     }
     
+    public float[] getDroneInputs() {
+        return droneInputs;
+    }
+    
+    private float yawPID(float yawDes, float yawAct) {
+        yawErr = yawDes - yawAct;
+        yawErrSum += yawErr*TIME_SHIFT;
+        yawDerr = (yawErr - prevYawErr)/TIME_SHIFT;
+        yawSpeedOut = kpYaw*yawErr + kiYaw*yawErrSum + kdYaw*yawDerr;
+        prevYawErr = yawErr;
+        return yawSpeedOut;
+    }
+    public void setYawTuning(float Kp, float Ki, float Kd) {
+        kpYaw = Kp;
+        kiYaw = Ki;
+        kdYaw = Kd;        
+    }
+    
+    private float zPID(float zDes, float zAct) {
+        zErr = zDes - zAct;
+        zErrSum += zErr*TIME_SHIFT;
+        zDerr = (zErr - prevZerr)/TIME_SHIFT;
+        zSpeedOut = kpZ*zErr + kiZ*zErrSum + kdZ*zDerr;
+        prevZerr = zErr;
+        return zSpeedOut;
+        
+    }
+    private void run() {
+        
+    }
 }
