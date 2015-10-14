68f3a14927753413071fdbfb3b72fba6798d2a0e
vegkams <vegard.kamsvag@stud.hials.no>
2015-10-14 15:07:37 +0200

    Started on PID-algorithms, work in progress

--- PS3ControllerData/src/yadrone/Regulator.java
+++ PS3ControllerData/src/yadrone/Regulator.java
@@ -36,8 +36,16 @@ public class Regulator {
     private float zSpeedOut;
     private final float TIME_SHIFT;
     
+    private final double CIRCLE_DIAMETER = 83;
+    private final double dev = 1.1;
+
     public Regulator() {
+
         TIME_SHIFT = 0.1f;
+
+        rawCoordinates = new ArrayList<>();
+        filteredCoordinates = new ArrayList<>();
+
     }
 
     /**
@@ -46,12 +54,16 @@ public class Regulator {
      *
      */
     public void CoordinateFilter() {
-        
-        int elements = 6;
+
+        int elements = 9;
         double[] avg = new double[3];
+        double[] stdD = new double[3];
         double sumX = 0;
         double sumY = 0;
         double sumR = 0;
+        if (rawCoordinates.size() > 10) {
+            rawCoordinates.remove(0);
+        }
         for (int i = 0; i < (elements - 1); i++) {
             sumX = sumX + (double) rawCoordinates.get(i).get(0, 0)[0];
             sumY = sumY + (double) rawCoordinates.get(i).get(0, 0)[1];
@@ -60,35 +72,47 @@ public class Regulator {
         avg[0] = sumX / elements;
         avg[1] = sumY / elements;
         avg[2] = sumR / elements;
+
         filteredCoordinates.add(avg);
+        for (int x = 0; x < (elements - 1); x++) {
+            for (int i = 0; i < 3; i++) {
+                if ((double) rawCoordinates.get(x).get(0, 0)[i] > avg[i] * dev
+                        || (double) rawCoordinates.get(x).get(0, 0)[i] < avg[i] / dev) {
+                    rawCoordinates.remove(x);
+                }
+            }
+        }
+
         if (rawCoordinates.size() > elements) {
             rawCoordinates.remove(0);
         }
+
     }
 
     /**
      * Add a new raw coordinate
+     *
      * @param v
      */
-    public void AddNewCoordinate(Mat v) {
+    public synchronized void AddNewCoordinate(Mat v) {
         if (v.cols() == 1 && v.rows() == 1) {
             rawCoordinates.add(v);
         }
+        CoordinateFilter();
     }
-    public Mat getLatestCoordinate(){
+
+    public synchronized Mat getLatestCoordinate() {
         Mat mat = new Mat();
-        if(filteredCoordinates.size()> 5){
-            filteredCoordinates.remove(0);
-        }
-        mat.put(0, 0, filteredCoordinates.get(filteredCoordinates.size()-1));
+        mat.put(0, 0, filteredCoordinates.get(filteredCoordinates.size() - 1));
         return mat;
     }
-    
-    public void DistanceEstimate(double[] circleinfo){
-        double realRad = CircleDiameter/2;
+
+    public void DistanceEstimate(double[] circleinfo) {
+        double realRad = CIRCLE_DIAMETER / 2;
         double virtualRadius = circleinfo[3];
-        
+
     }
+
     
     public float[] getDroneInputs() {
         return droneInputs;
@@ -120,4 +144,5 @@ public class Regulator {
     private void run() {
         
     }
+
 }
