c1714d2a22c1847a1f4c22a2013165966febbbde
vegkams <vegard.kamsvag@stud.hials.no>
2015-11-11 18:43:28 +0100

    Syklustid regulator = 50 ms

--- PS3ControllerData/src/yadrone/YADrone.java
+++ PS3ControllerData/src/yadrone/YADrone.java
@@ -24,7 +24,7 @@ import java.util.concurrent.Semaphore;
 public class YADrone
 {
 
-    static final int PERIOD = 100;
+    static final int PERIOD = 50;
     static final boolean IS_RESOLUTION_HIGH = false;
     static IARDrone drone;
     static PS3ControllerReader reader;
